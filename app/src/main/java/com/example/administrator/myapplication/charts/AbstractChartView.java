3fc6e71b352e8771dc235625c397e3287f90a247
wudashan <453685754@qq.com>
2015-11-19 17:56:15 +0800

    2015年11月19日17:56:10

--- app/src/main/java/com/example/administrator/myapplication/charts/AbstractChartView.java
+++ app/src/main/java/com/example/administrator/myapplication/charts/AbstractChartView.java
@@ -1,6 +1,7 @@
 package com.example.administrator.myapplication.charts;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
@@ -8,6 +9,9 @@ import android.os.Vibrator;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.ViewConfiguration;
+
+import com.example.administrator.myapplication.R;
 
 
 /**
@@ -27,6 +31,7 @@ public abstract class AbstractChartView<T extends ChartData> extends RefreshLoad
     private float downX;
     private float downY;
     public float dpUnit;
+    public int fontColor;
     public float fontSize;
     private boolean fullscreen = false;
     public int gridColor;
@@ -76,9 +81,57 @@ public abstract class AbstractChartView<T extends ChartData> extends RefreshLoad
     protected abstract void onCursor(Canvas paramCanvas, T paramT, MotionEvent paramMotionEvent);
 
 
-    public AbstractChartView(Context paramContext) {
+    public AbstractChartView(Context paramContext, String paramString1, String paramString2) {
         super(paramContext);
+        this.market = paramString1;
+        this.code = paramString2;
+        Resources localResources = paramContext.getResources();
+        this.dpUnit = localResources.getDimension(R.dimen.dpUnit);
+        this.marginVertical = localResources.getDimension(R.dimen.chart_margin_vertical);
+        this.marginHorizontal = localResources.getDimension(R.dimen.chart_margin_horizontal);
+        this.span = localResources.getDimension(R.dimen.chart_span);
+        this.upColor = localResources.getColor(R.color.chart_red_color);
+        this.downColor = localResources.getColor(R.color.chart_green_color);
+        this.fontColor = localResources.getColor(R.color.chart_font_color);
+        this.fontSize = localResources.getDimension(R.dimen.chart_font_size);
+        this.loadingFontSize = localResources.getDimension(R.dimen.chart_loading_font_size);
+        this.axisColor = localResources.getColor(R.color.chart_axis_color);
+        this.gridColor = localResources.getColor(R.color.chart_grid_color);
+        this.indicatorColor = localResources.getColor(R.color.chart_indicator_color);
+        this.priceAreaColor = localResources.getColor(R.color.chart_time_price_area_color);
+        this.priceLineColor = localResources.getColor(R.color.chart_time_price_line_color);
+        this.avgPriceLineColor = localResources.getColor(R.color.chart_time_avg_price_line_color);
+        this.labelSpan = 3 * this.dpUnit;
+        setOnClickListener(this);
+        this.mScaleDetector = new ScaleGestureDetector(getContext(), new ScaleGestureDetector.OnScaleGestureListener() {
+            @Override
+            public boolean onScale(ScaleGestureDetector detector) {
+                if (chartData != null && detector.getScaleFactor() > 0.01){
+                    onZoom(canvas, chartData, detector.getScaleFactor());
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onScaleBegin(ScaleGestureDetector detector) {
+                return true;
+            }
+
+            @Override
+            public void onScaleEnd(ScaleGestureDetector detector) {
+            }
+        });
+        this.paint = new Paint();
+        this.paint.setAntiAlias(true);
+        this.vibrator = (Vibrator) paramContext.getSystemService(Context.VIBRATOR_SERVICE);
+        ViewConfiguration localViewConfiguration = ViewConfiguration.get(paramContext);
+        this.longPressTimeOut = 400;
+        this.mTouchSlopSquare = localViewConfiguration.getScaledTouchSlop() * localViewConfiguration.getScaledTouchSlop();
+
     }
+
+    protected abstract void onZoom(Canvas paramCanvas, T paramT, float paramFloat);
 
     @Override
     public void onClick(View v) {
--- app/src/main/java/com/example/administrator/myapplication/charts/time/TimeTodayChartView.java
+++ app/src/main/java/com/example/administrator/myapplication/charts/time/TimeTodayChartView.java
@@ -2,10 +2,14 @@ package com.example.administrator.myapplication.charts.time;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Path;
 import android.view.MotionEvent;
 
 import com.example.administrator.myapplication.charts.AbstractChartView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wudashan on 2015/11/18 0018.
@@ -14,6 +18,27 @@ public class TimeTodayChartView extends AbstractChartView<TimeTodayChartData>{
 
     private Path avgPricePath = new Path();
 //    private CharView chartView;
+    private TimeChartCursor cursor;
+    public float height;
+    private List<TimeChartCursor> items = new ArrayList<>();
+    public float left;
+    private float lowerLimit;
+    private float maxX;
+    private Paint paint;
+    private Path path = new Path();
+    public float priceAreaBottom;
+    private Path priceAreaPath = new Path();
+    public float priceAreaTop;
+    private String[] priceLabels = new String[5];
+    private Path pricePath = new Path();
+    private String[] pricePercentLabels = new String[5];
+    public float right;
+    private float upperLimit;
+    public float volumeAreaBottom;
+    public float volumeAreaTop;
+    private String[] volumeLabels = new String[4];
+    public float width;
+
 
 
 
@@ -22,8 +47,15 @@ public class TimeTodayChartView extends AbstractChartView<TimeTodayChartData>{
 
     }
 
-    public TimeTodayChartView(Context context) {
-        super(context);
+    public TimeTodayChartView(Context context, String paramString1, String paramString2) {
+        super(context, paramString1, paramString2);
+        this.paint = new Paint();
+        this.paint.setAntiAlias(true);
+        this.paint.setTextSize(this.fontSize);
+    }
+
+    private void drwaOutline(Canvas paramCanvas, TimeTodayChartData paramTimeTodayChartData, Paint paramPaint){
+
     }
 
     public void redrawChart(){
