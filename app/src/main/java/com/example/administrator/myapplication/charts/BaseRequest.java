8773b467ea6d5b3348707db823631c09f2393910
wudashan <453685754@qq.com>
2015-11-26 14:27:25 +0800

    new feature start

--- app/src/main/java/com/example/administrator/myapplication/charts/BaseRequest.java
+++ app/src/main/java/com/example/administrator/myapplication/charts/BaseRequest.java
@@ -21,7 +21,6 @@ import java.util.Map;
  */
 public class BaseRequest<T> extends Request<T>{
     //new feature start
-
     private static final String TAG = "BaseRequest";
     private static String UA_APP_SUFFIX = null;
     private static String UA_DEFAULT = System.getProperty("http.agent", "");
